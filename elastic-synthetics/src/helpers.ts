0496d96385d98c12693cc6c84a6985136a91d7aa
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2020-08-20 13:18:26 +0200

    feat: enhance cli runner with symbols and stats

--- elastic-synthetics/src/helpers.ts
+++ elastic-synthetics/src/helpers.ts
@@ -1,7 +1,23 @@
-import { blueBright } from 'chalk';
+import { red, green, yellow, grey } from 'kleur';
 
 export function debug(message: string) {
     if (process.env.DEBUG) {
-        process.stdout.write(blueBright(message + '\n'));
+        process.stdout.write(grey(message + '\n'));
     }
 }
+
+export function indent(lines: string, tab: string = '   ') {
+    return lines.replace(/^/gm, tab);
+}
+
+export const symbols = {
+    warning: yellow('⚠'),
+    pass: green('✓'),
+    fail: red('✖')
+};
+
+export function getMilliSecs(startTime: [number, number]) {
+    // [seconds, nanoseconds]
+    const hrTime = process.hrtime(startTime);
+    return Math.trunc(hrTime && hrTime[0] * 1e3 + hrTime[1] / 1e6)
+};
