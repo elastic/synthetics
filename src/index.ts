deb5316ca0c9a174780d7b6941befc3d21934c88
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2020-08-26 12:52:07 +0200

    chore: control environment through NODE_ENV

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@ import { RunOptions } from './dsl/runner';
 require('source-map-support').install();
 
 export async function run(options: RunOptions) {
+  /**
+   * Use the NODE_ENV variable to control the environment if its not explicity
+   * passed from either CLI or through the API
+   */
+  options.environment = options.environment || process.env['NODE_ENV'];
+
   try {
     await runner.run(options);
   } catch (e) {
