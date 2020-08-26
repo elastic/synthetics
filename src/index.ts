54e5ed478701adcd0ed8c0a6feae197335652a65
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2020-08-26 12:46:07 +0200

    fix: rewamp the cli and api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import { runner } from './dsl';
+import { RunOptions } from './dsl/runner';
+
 /* eslint-disable @typescript-eslint/no-var-requires */
 require('source-map-support').install();
 
+export async function run(options: RunOptions) {
+  try {
+    await runner.run(options);
+  } catch (e) {
+    console.error('Failed to run the test', e);
+    process.exit(1);
+  }
+}
+
 export * from './dsl';
-export { run, program } from './cli';
