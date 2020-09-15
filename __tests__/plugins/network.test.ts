f78dabe613429189360ffe1330b2d73eb5b4260f
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2020-09-15 23:09:00 +0200

    add local server for testing

--- __tests__/plugins/network.test.ts
+++ __tests__/plugins/network.test.ts
@@ -2,17 +2,25 @@ import fs from 'fs';
 import { runner, step, journey } from '../../src/dsl/index';
 import { generateTempPath } from '../../src/helpers';
 import { NetworkManager } from '../../src/plugins/network';
+import { Server } from '../utils/server';
 
 describe('network', () => {
   const dest = generateTempPath();
-  afterAll(() => fs.unlinkSync(dest));
+  let server: Server;
+  beforeAll(async () => {
+    server = await Server.create();
+  });
+  afterAll(async () => {
+    fs.unlinkSync(dest);
+    await server.close();
+  });
 
   it('should capture network info', async () => {
     journey('Test', () => {
       step('net info', async ({ client, page }) => {
         const network = new NetworkManager();
         await network.start(client);
-        await page.goto('https://vigneshh.in');
+        await page.goto(server.TEST_PAGE);
         const netinfo = await network.stop();
         expect(netinfo.length).toBeGreaterThan(0);
       });
--- __tests__/plugins/tracing.test.ts
+++ __tests__/plugins/tracing.test.ts
@@ -2,11 +2,17 @@ import fs from 'fs';
 import { runner, step, journey } from '../../src/dsl/index';
 import { Tracing } from '../../src/plugins/tracing';
 import { generateTempPath } from '../../src/helpers';
+import { Server } from '../utils/server';
 
 describe('tracing', () => {
   const dest = generateTempPath();
-  afterAll(() => {
+  let server: Server;
+  beforeAll(async () => {
+    server = await Server.create();
+  });
+  afterAll(async () => {
     fs.unlinkSync(dest);
+    await server.close();
   });
 
   it('should capture filmstrips', async () => {
@@ -14,7 +20,7 @@ describe('tracing', () => {
       step('capture filmstrips', async ({ client, page }) => {
         const tracer = new Tracing();
         await tracer.start(client);
-        await page.goto('https://vigneshh.in');
+        await page.goto(server.TEST_PAGE);
         const events = await tracer.stop(client);
         expect(events.toString('utf-8')).toContain('screenshot');
       });
