ad582e17606d5d08b9412521d09f38504d174c5b
Andrew Cholakian <andrew@andrewvc.com>
2020-09-30 19:47:24 -0500

    fix tests

--- __tests__/reporters/json.test.ts
+++ __tests__/reporters/json.test.ts
@@ -1,18 +1,37 @@
 import fs from 'fs';
 import { runner, step, journey } from '../../src/core';
 import JSONReporter from '../../src/reporters/json';
-import { generateTempPath } from '../../src/helpers';
+import { formatError, generateTempPath } from '../../src/helpers';
+import { Journey } from '../../src/dsl';
 
 describe('json reporter', () => {
-  const dest = generateTempPath();
-  afterAll(() => {
+  let dest: string;
+  let j1: Journey;
+  let stream;
+
+  beforeEach(() => {
+    dest = generateTempPath();
+    stream = new JSONReporter(runner, { fd: fs.openSync(dest, 'w') }).stream;
+    jest.useFakeTimers('modern').setSystemTime(new Date('2020-09-17'));
+    j1 = journey('j1', () => {});
+  });
+
+  afterEach(() => {
     fs.unlinkSync(dest);
   });
 
+  const readAndCloseStream = async () => {
+    /**
+     * Close the underyling stream writing to FD to read all the contents
+     */
+    stream.end();
+    await new Promise(resolve => stream.once('finish', resolve));
+    const fd = fs.openSync(dest, 'r');
+    const buffer = fs.readFileSync(fd, 'utf-8');
+    return buffer;
+  };
+
   it('writes each step as NDJSON to the FD', async () => {
-    const { stream } = new JSONReporter(runner, { fd: fs.openSync(dest, 'w') });
-    jest.useFakeTimers('modern').setSystemTime(new Date('2020-09-17'));
-    const j1 = journey('j1', () => {});
     runner.emit('journey:start', {
       journey: j1,
       params: {},
@@ -28,24 +47,13 @@ describe('json reporter', () => {
       start: 0,
       end: 10,
     });
-    runner.emit('step:end', {
-      journey: j1,
-      status: 'failed',
-      step: step('s2', async () => {}),
-      screenshot: 'dummy2',
-      url: 'dummy2',
-      timestamp: 1600300800000001,
-      start: 11,
-      end: 20,
-      error: new Error('myError'),
-    });
     runner.emit('journey:end', {
       journey: j1,
       params: {},
       status: 'succeeded',
       start: 0,
-      end: 20,
-      timestamp: 1600300800000002,
+      end: 10,
+      timestamp: 1600300800000000,
       filmstrips: [
         {
           snapshot: 'dummy',
@@ -63,13 +71,36 @@ describe('json reporter', () => {
     });
     runner.emit('end', 'done');
 
-    /**
-     * Close the underyling stream writing to FD to read all the contents
-     */
-    stream.end();
-    await new Promise(resolve => stream.once('finish', resolve));
-    const fd = fs.openSync(dest, 'r');
-    const buffer = fs.readFileSync(fd, 'utf-8');
-    expect(buffer.toString()).toMatchSnapshot();
+    expect((await readAndCloseStream()).toString()).toMatchSnapshot();
+  });
+
+  it('writes errors to the top level', async () => {
+    const myErr = new Error('myError');
+
+    runner.emit('step:end', {
+      journey: j1,
+      status: 'failed',
+      step: step('s2', async () => {}),
+      screenshot: 'dummy2',
+      url: 'dummy2',
+      timestamp: 1600300800000001,
+      start: 11,
+      end: 20,
+      error: myErr,
+    });
+
+    const buffer = await readAndCloseStream();
+    let error: any;
+    buffer.split('\n').forEach(l => {
+      try {
+        const parsed = JSON.parse(l);
+        if (parsed.type == 'step/end') {
+          error = parsed.error;
+        }
+      } catch (e) {
+        return; // ignore empty lines
+      }
+    });
+    expect(error).toEqual(formatError(myErr));
   });
 });
