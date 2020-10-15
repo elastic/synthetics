e4661d7776b19800d9fe21ca0a03eb498b03ee00
Andrew Cholakian <andrew@andrewvc.com>
2020-10-14 20:40:18 -0500

    Improvements

--- examples/docker/run.sh
+++ examples/docker/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker pull docker.elastic.co/observability-ci/synthetics:master-7.10.0-synthetics
+docker pull docker.elastic.co/observability-ci/synthetics:master-8.0.0-synthetics
 docker run \
   --rm \
   --name=heartbeat \
@@ -9,7 +9,7 @@ docker run \
   --volume="$(pwd)/heartbeat.docker.yml:/usr/share/heartbeat/heartbeat.yml:ro" \
   --volume="$(pwd)/../sample-app/journeys/dist:/opt/sample-app:ro" \
   --volume="$(pwd)/../elastic-docs/dist:/opt/elastic-docs:ro" \
-  docker.elastic.co/observability-ci/synthetics:master-7.10.0-synthetics \
+  docker.elastic.co/observability-ci/synthetics:master-8.0.0-synthetics \
   --strict.perms=false -e \
   -E output.elasticsearch.hosts=["localhost:9200"] \
   -E output.elasticsearch.username=elastic \
