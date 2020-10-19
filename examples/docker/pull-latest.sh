e58781810332ffe5d5dce3bf24e472f9a436ae32
Andrew Cholakian <andrew@andrewvc.com>
2020-10-19 14:41:38 -0500

    Fix versions to 7.10.0

--- examples/docker/pull-latest.sh
+++ examples/docker/pull-latest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker pull docker.elastic.co/observability-ci/synthetics:master-7.11.0-synthetics
+docker pull docker.elastic.co/observability-ci/synthetics:master-7.10.0-synthetics
 docker pull docker.elastic.co/observability-ci/synthetics:master-8.0.0-synthetics
-docker pull  docker.elastic.co/observability-ci/heartbeat:7.11.0-synthetics
+docker pull  docker.elastic.co/observability-ci/heartbeat:7.10.0-synthetics
 docker pull  docker.elastic.co/observability-ci/heartbeat:8.0.0-synthetics
