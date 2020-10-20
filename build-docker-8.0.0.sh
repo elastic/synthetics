0045619216a01a958cf65c6e6fd1818d8022e76a
Andrew Cholakian <andrew@andrewvc.com>
2020-10-20 11:45:12 -0500

    Better version syntax for build-docker

--- build-docker-8.0.0.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./build-docker.sh 8.0.0-synthetics
--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-STACK_VERSION=${1:-7.10.0-synthetics}
+STACK_VERSION=${1:-7.10.0}
 echo "Building docker image based on ${STACK_VERSION}..."
 npm i
 npm run build
 npm pack
-docker build . -t heartbeat-synthetics-local --build-arg STACK_VERSION=$STACK_VERSION
+docker build . -t heartbeat-synthetics-local --build-arg STACK_VERSION=$STACK_VERSION-synthetics
