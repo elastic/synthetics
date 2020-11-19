30c284379a2770a6580fa65d09d8bf929320d9de
Shahzad <shahzad31comp@gmail.com>
2020-11-19 16:15:20 +0100

    added health checks

--- __tests__/e2e/scripts/test.sh
+++ __tests__/e2e/scripts/test.sh
@@ -1,28 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-# variables
-KIBANA_PORT=5601
-
-# ensure Docker is running
-docker ps &> /dev/null
-if [ $? -ne 0 ]; then
-    echo "⚠️  Please start Docker"
-    exit 1
-fi
 
 # formatting
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-# Wait for Kibana to start
-##################################################
-echo "" # newline
-echo "${bold}Waiting for Kibana to start...${normal}"
-yarn wait-on -i 500 -w 500 http-get://admin:changeme@localhost:$KIBANA_PORT/api/status > /dev/null
 
-
-# Wait for Heartbeat docker to start
+# Wait for synthetics docker to start
 ##################################################
 echo "" # newline
 echo "${bold}Waiting for synthetics docker to start...${normal}"
