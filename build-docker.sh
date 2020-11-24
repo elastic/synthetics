5956665ebd7b61d257ef17432f0d0ac583f9df0a
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2020-11-24 21:26:24 +0530

    fix: sync browser version in docker and examples

--- build-docker.sh
+++ build-docker.sh
@@ -2,6 +2,7 @@
 set -e
 set -x
 STACK_VERSION=${1:-7.10.0}
-echo "Building docker image based on ${STACK_VERSION}..."
+echo "Installing package dependencies for running local build"
 npm i
+echo "Building docker image based on ${STACK_VERSION}..."
 docker build . -t heartbeat-synthetics-local --build-arg STACK_VERSION=$STACK_VERSION-synthetics
