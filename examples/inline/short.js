bae989e587e1c04ee9f335ba3fe066fc81c15817
Shahzad <shahzad31comp@gmail.com>
2020-12-09 15:55:17 +0100

    revert

--- examples/inline/short.js
+++ examples/inline/short.js
@@ -1,3 +1,6 @@
 step('load homepage', async () => {
   await page.goto('https://www.elastic.co');
 });
+step('hover over products menu', async () => {
+  await page.hover('css=[data-nav-item=products]');
+});
