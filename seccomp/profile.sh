9038f22fff163794b54249e78640a9abb208a39f
Hamid <h.jahtalab@gmail.com>
2021-01-19 15:01:56 +0100

    chore: copy synthetics repo into seccomp docker image
    minor clean ups

--- seccomp/profile.sh
+++ seccomp/profile.sh
@@ -1,6 +1,6 @@
 # TODO: use docker syslog driver instead
 service rsyslog start
-npm install
+# npm run build
 
 go build -o ./build/seccomp_exec seccomp_exec.go
 
@@ -8,5 +8,9 @@ node ../dist/browser-service.js &
 tail -f /var/log/syslog > ./build/syslog &
 
 ./build/seccomp_exec -policy=./seccomp_log_agent.yml node ../dist/cli.js ../examples/todos --ws-endpoint=ws://localhost:9322
+
+./build/seccomp_exec -policy=./seccomp_log_agent.yml cat ../examples/inline/sample-inline-journey.js | node ../dist/cli.js --inline --ws-endpoint=ws://localhost:9322
+
+service rsyslog stop
 
 node parse-syslog.js ./build/syslog > synthetics_agent_profile.yml
