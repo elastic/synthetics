elastic/synthetics

commit 2f62fab97995b0c8f0d3a44b6e614b0576206b11

Fix linted shellcheck defects (#189)

examples/docker/run.sh
@@ -1,36 +1,34 @@
-#!/bin/bash
-set -e
-set -x
+#!/usr/bin/env bash
+set -ex
 VERSION=${1:-7.10.0}
-if [ ! -z $1 ]; then
+if [ -n "${1}" ]; then
   shift # discard first arg
 fi
-HEARTBEAT_ARGS=$@
 
-if [ -z $1 ]; then
-  HEARTBEAT_ARGS="-E output.elasticsearch.hosts=["localhost:9200"] -E output.elasticsearch.username=elastic -E output.elasticsearch.password=changeme"
+if [ -z "${1}" ]; then
+  HEARTBEAT_ARGS="-E output.elasticsearch.hosts=[\"localhost:9200\"] -E output.elasticsearch.username=elastic -E output.elasticsearch.password=changeme"
 else
-  HEARTBEAT_ARGS="$@"
+  HEARTBEAT_ARGS="$*"
 fi
 
 # Set Image based on version
-if [[ $VERSION =~ ^[0-9] ]]; then
-	IMAGE=docker.elastic.co/experimental/synthetics:$VERSION-synthetics
+if [[ ${VERSION} =~ ^[0-9] ]]; then
+	IMAGE="docker.elastic.co/experimental/synthetics:${VERSION}-synthetics"
 else
-	IMAGE=$VERSION
+	IMAGE=${VERSION}
 fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "Using image '$IMAGE' with extra args: $HEARTBEAT_ARGS"
+echo "Using image '${IMAGE}' with extra args: ${HEARTBEAT_ARGS}"
 docker run \
   --rm \
   --name=heartbeat \
   --user=heartbeat \
   --net=host \
-  --security-opt seccomp=$SCRIPT_DIR/seccomp_profile.json \
+  --security-opt seccomp="${SCRIPT_DIR}"/seccomp_profile.json \
   --volume="$(pwd)/heartbeat.docker.yml:/usr/share/heartbeat/heartbeat.yml:ro" \
   --volume="$(pwd)/../../:/opt/elastic-synthetics:rw" \
-  $IMAGE \
+  "${IMAGE}" \
   --strict.perms=false -e \
-  $HEARTBEAT_ARGS
+  "${HEARTBEAT_ARGS}"

.ci/Jenkinsfile
@@ -62,6 +62,7 @@ pipeline {
           withNodeEnv(){
             dir("${BASE_DIR}"){
               sh(label: 'Checks linting',script: 'npm run-script lint')
+              preCommit(commit: "${GIT_BASE_COMMIT}", junit: true)
             }
           }
         }

.pre-commit-config.yaml
@@ -1,0 +1,22 @@
+repos:
+-   repo: git://github.com/pre-commit/pre-commit-hooks
+    rev: v2.2.3
+    hooks:
+    -   id: check-case-conflict
+    -   id: check-executables-have-shebangs
+    -   id: check-json
+    -   id: check-merge-conflict
+    -   id: check-xml
+    -   id: check-yaml
+    -   id: end-of-file-fixer
+
+-   repo: git@github.com:elastic/apm-pipeline-library
+    rev: current
+    hooks:
+    -   id: check-bash-syntax
+    -   id: check-en-dashes
+    -   id: check-jenkins-pipelines
+    -   id: check-jjbb
+    -   id: check-unicode-non-breaking-spaces
+    -   id: remove-en-dashes
+    -   id: remove-unicode-non-breaking-spaces

examples/docker/bash-build-local.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Build a local docker image using the latest source in this repo
 # and run a bash prompty on the container
 pushd ../../

examples/docker/bash.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 VERSION=${1:-7.10.0}
-if [[ $VERSION =~ ^[0-9] ]]; then
-	IMAGE=docker.elastic.co/experimental/synthetics:$VERSION-synthetics
+if [[ ${VERSION} =~ ^[0-9] ]]; then
+	IMAGE="docker.elastic.co/experimental/synthetics:${VERSION}-synthetics"
 else
-	IMAGE=$VERSION
+	IMAGE=${VERSION}
 fi
-echo "Using image $IMAGE"
+echo "Using image ${IMAGE}"
 docker run \
   -it \
   --rm \
@@ -15,5 +15,5 @@ docker run \
   --security-opt seccomp=seccomp_profile.json \
   --volume="$(pwd)/heartbeat.docker.yml:/usr/share/heartbeat/heartbeat.yml:ro" \
   --volume="$(pwd)/../../:/opt/elastic-synthetics:rw" \
-  $IMAGE \
+  "${IMAGE}" \
   bash

examples/docker/pull-latest.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 docker pull docker.elastic.co/experimental/synthetics:7.10.0-synthetics
 docker pull docker.elastic.co/experimental/synthetics:8.0.0-synthetics
 docker pull docker.elastic.co/observability-ci/heartbeat:7.10.0-synthetics

examples/docker/run-build-local.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Build a local docker image using the latest source in this repo
 # and run heartbeat with the config in this directory
 pushd ../../
-./build-docker.sh $1
-popd
+./build-docker.sh "${1}"
+popd || exit
 ./run.sh heartbeat-synthetics-local "${@:2}"

commit 496be9f1439244d8eb3fdccbc5078064ced74b40

Fix broken overwrite of heartbeat args
This was broken in https://github.com/elastic/synthetics/pull/189
which quoted this variable that should not have been quoted

examples/docker/run.sh
@@ -31,4 +31,4 @@ docker run \
   --volume="$(pwd)/../../:/opt/elastic-synthetics:rw" \
   "${IMAGE}" \
   --strict.perms=false -e \
-  "${HEARTBEAT_ARGS}"
+  ${HEARTBEAT_ARGS}