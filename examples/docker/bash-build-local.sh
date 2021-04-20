c2b48b3f29fcb29254e325a5a4de1ac991b20d4e
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2021-04-20 07:36:09 -0700

    chore: use official docker image and cleanup

--- examples/docker/bash-build-local.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Build a local docker image using the latest source in this repo
-# and run a bash prompty on the container
-pushd ../../
-./build-docker.sh $1
-popd
-./bash.sh heartbeat-synthetics-local
