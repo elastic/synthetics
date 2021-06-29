8c59b3cae631ff65d6dac844ab090fb76264462e
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2021-06-29 09:47:06 -0700

    fix: add fallback for terminal w/o utf-8 support

--- src/helpers.ts
+++ src/helpers.ts
@@ -41,11 +41,12 @@ export function indent(lines: string, tab = '   ') {
   return lines.replace(/^/gm, tab);
 }
 
+const NO_UTF8_SUPPORT = process.platform === 'win32' && !process.env.WT_SESSION;
 export const symbols = {
-  warning: yellow('⚠'),
+  warning: yellow(NO_UTF8_SUPPORT ? '!' : '⚠'),
   skipped: cyan('-'),
-  succeeded: green('✓'),
-  failed: red('✖'),
+  succeeded: green(NO_UTF8_SUPPORT ? 'ok' : '✓'),
+  failed: red(NO_UTF8_SUPPORT ? 'x' : '✖'),
 };
 
 export function generateUniqueId() {
