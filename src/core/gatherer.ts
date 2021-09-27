ec3816fde03c78b328ffdd4efb34b5a2d901e17a
Dominique Clarke <dominique.clarke@elastic.co>
2021-09-27 15:06:58 -0400

    set network conditions on new page

--- src/core/gatherer.ts
+++ src/core/gatherer.ts
@@ -23,7 +23,7 @@
  *
  */
 
-import { chromium, ChromiumBrowser } from 'playwright-chromium';
+import { chromium, ChromiumBrowser, BrowserContext } from 'playwright-chromium';
 import { PluginManager } from '../plugins';
 import { RunOptions } from './runner';
 import { log } from './logger';
@@ -55,11 +55,10 @@ export class Gatherer {
       ...playwrightOptions,
       userAgent: await Gatherer.getUserAgent(),
     });
+    await Gatherer.setNetworkConditions(context, networkConditions);
+
     const page = await context.newPage();
     const client = await context.newCDPSession(page);
-    if (networkConditions) {
-      await client.send('Network.emulateNetworkConditions', networkConditions);
-    }
     return { browser: Gatherer.browser, context, page, client };
   }
 
@@ -67,6 +66,16 @@ export class Gatherer {
     const session = await Gatherer.browser.newBrowserCDPSession();
     const { userAgent } = await session.send('Browser.getVersion');
     return userAgent + ' Elastic/Synthetics';
+  }
+
+  static async setNetworkConditions(context: BrowserContext, networkConditions: RunOptions['networkConditions']) {
+    if (networkConditions) {
+      context.on('page', async (page) => {
+        const context = page.context();
+        const client = await context.newCDPSession(page);
+        await client.send('Network.emulateNetworkConditions', networkConditions);
+      });
+    }
   }
 
   /**
