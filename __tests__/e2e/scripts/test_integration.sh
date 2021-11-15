dc37c00223c3d7e44d2bb64cbc87c6d754a966ba
Dominique Clarke <dominique.clarke@elastic.co>
2021-11-14 23:57:50 -0500

    add synthetics_e2e args

--- __tests__/e2e/scripts/test_integration.sh
+++ __tests__/e2e/scripts/test_integration.sh
@@ -1,9 +1,11 @@
+SYNTHETICS_E2E_ARGS="${@:2}"
+
 #!/usr/bin/env bash
 set -e pipefail
 
 # run e2e tests journey
 ##################################################
-npx @elastic/synthetics synthetics.journey.ts --no-headless
+npx @elastic/synthetics synthetics.journey.ts $SYNTHETICS_E2E_ARGS
 
 # Take the stack down
 elastic-package stack down
--- __tests__/e2e/scripts/test_versions.sh
+++ __tests__/e2e/scripts/test_versions.sh
@@ -3,7 +3,9 @@ set -e
 
 input="versions"
 
+SYNTHETICS_E2E_ARGS="${@:2}"
+
 while IFS= read -r line
 do
-  sh ./scripts/setup_integration.sh $line && sh ./scripts/$1_integration.sh $line
+  sh ./scripts/setup_integration.sh $line && sh ./scripts/$1_integration.sh $line $SYNTHETICS_E2E_ARGS
 done < "$input"
