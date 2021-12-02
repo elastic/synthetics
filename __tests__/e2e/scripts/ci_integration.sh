449a389eb34b2226b89a0c9c2a9fb3a8423bc636
Dominique Clarke <dominique.clarke@elastic.co>
2021-12-02 16:48:45 -0500

    add synthetics args

--- __tests__/e2e/scripts/ci_integration.sh
+++ __tests__/e2e/scripts/ci_integration.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
+SYNTHETICS_E2E_ARGS=$2
+
+if [ -z "${JENKINS_URL}" ]; then
+  # formatting
+  bold=$(tput bold)
+  normal=$(tput sgr0)
+  SLEEP_TIME="0.1"
+else
+  SLEEP_TIME="10"
+fi
+
 # run e2e tests journey
 ##################################################
-SYNTHETICS_JUNIT_FILE="junit_$1.xml" npx @elastic/synthetics synthetics.journey.ts --reporter junit
+STACK_VERSION=$1 SYNTHETICS_JUNIT_FILE="junit_$1.xml" npx @elastic/synthetics synthetics.journey.ts --reporter junit $SYNTHETICS_E2E_ARGS
 
 # Take the stack down
 elastic-package stack down
