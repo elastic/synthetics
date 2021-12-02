80e736314cd2eff12440674675740213e9fbfb1a
Dominique Clarke <dominique.clarke@elastic.co>
2021-12-02 16:06:10 -0500

    remove formatting

--- __tests__/e2e/scripts/ci_integration.sh
+++ __tests__/e2e/scripts/ci_integration.sh
@@ -3,15 +3,6 @@ set -e
 
 SYNTHETICS_E2E_ARGS="${@:2}"
 
-if [ -z "${JENKINS_URL}" ]; then
-  # formatting
-  bold=$(tput bold)
-  normal=$(tput sgr0)
-  SLEEP_TIME="0.1"
-else
-  SLEEP_TIME="10"
-fi
-
 # run e2e tests journey
 ##################################################
 SYNTHETICS_JUNIT_FILE="junit_$1.xml" npx @elastic/synthetics synthetics.journey.ts --reporter junit $SYNTHETICS_E2E_ARGS
