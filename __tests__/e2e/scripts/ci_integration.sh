5784d1b180753c1df72e6cda21e174947ef93c24
Dominique Clarke <dominique.clarke@elastic.co>
2021-12-02 15:46:04 -0500

    adjust shebangs

--- __tests__/e2e/scripts/ci_integration.sh
+++ __tests__/e2e/scripts/ci_integration.sh
@@ -1,7 +1,7 @@
-SYNTHETICS_E2E_ARGS="${@:2}"
-
 #!/usr/bin/env bash
 set -e
+
+SYNTHETICS_E2E_ARGS="${@:2}"
 
 if [ -z "${JENKINS_URL}" ]; then
   # formatting
@@ -19,4 +19,4 @@ SYNTHETICS_JUNIT_FILE="junit_$1.xml" npx @elastic/synthetics synthetics.journey.
 # Take the stack down
 elastic-package stack down
 
-docker rmi docker.elastic.co/beats/elastic-agent-complete:$1 docker.elastic.co/elasticsearch/elasticsearch:$1 docker.elastic.co/kibana/kibana:$1 elastic-package-stack_package-registry
+docker rmi docker.elastic.co/beats/elastic-agent-complete:$1 docker.elastic.co/elasticsearch/elasticsearch:$1 docker.elastic.co/kibana/kibana:$1 elastic-package-stack_package-registry:latest
--- __tests__/e2e/scripts/test_integration.sh
+++ __tests__/e2e/scripts/test_integration.sh
@@ -1,11 +1,11 @@
-SYNTHETICS_E2E_ARGS="${@:2}"
-
 #!/usr/bin/env bash
 set -e
 
+SYNTHETICS_E2E_ARGS="${@:2}"
+
 # run e2e tests journey
 ##################################################
-npx @elastic/synthetics synthetics.journey.ts $SYNTHETICS_E2E_ARGS
+STACK_VERSION=$1 npx @elastic/synthetics synthetics.journey.ts $SYNTHETICS_E2E_ARGS
 
 # Take the stack down
 elastic-package stack down
