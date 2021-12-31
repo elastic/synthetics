19b766b582a67034a89460697474e7cde4a1e93e
Alberto Delgado Roda <albert.delgadoroda@elastic.co>
2021-12-31 11:47:26 +0100

    chore: exclude 8.1.0-SNAPSHOT from CI (#433)

--- __tests__/e2e/scripts/test_versions.sh
+++ __tests__/e2e/scripts/test_versions.sh
@@ -7,5 +7,7 @@ SYNTHETICS_E2E_ARGS="${@:2}"
 
 while IFS= read -r line
 do
-  sh ./scripts/setup_integration.sh $line && sh ./scripts/$1_integration.sh $line $SYNTHETICS_E2E_ARGS
+  if [[ "$line" != *"DISABLED"* ]]; then
+      sh ./scripts/setup_integration.sh $line && sh ./scripts/$1_integration.sh $line $SYNTHETICS_E2E_ARGS
+  fi
 done < "$input"
