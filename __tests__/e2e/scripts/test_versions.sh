dab86c826f3c50032205e508384d0acbc842f24a
Alberto Delgado Roda <albert.delgadoroda@elastic.co>
2022-01-10 13:56:43 +0100

    chore: enable 8.1.0-SNAPSHOT (#440)

--- __tests__/e2e/scripts/test_versions.sh
+++ __tests__/e2e/scripts/test_versions.sh
@@ -7,7 +7,5 @@ SYNTHETICS_E2E_ARGS="${@:2}"
 
 while IFS= read -r line
 do
-  if [[ "$line" != *"DISABLED"* ]]; then
-      sh ./scripts/setup_integration.sh $line && sh ./scripts/$1_integration.sh $line $SYNTHETICS_E2E_ARGS
-  fi
+    sh ./scripts/setup_integration.sh $line && sh ./scripts/$1_integration.sh $line $SYNTHETICS_E2E_ARGS
 done < "$input"
