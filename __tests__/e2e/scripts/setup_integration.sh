6f1b604052c48af47e72cf6456615fea2b075f8c
Dominique Clarke <dominique.clarke@elastic.co>
2022-06-16 14:30:14 -0400

    adjust cluster allocation routing

--- __tests__/e2e/scripts/setup_integration.sh
+++ __tests__/e2e/scripts/setup_integration.sh
@@ -13,20 +13,11 @@ env ELASTICSEARCH_IMAGE_REF=$1 ELASTIC_AGENT_IMAGE_REF=$1 KIBANA_IMAGE_REF=$1 el
 
 curl -X PUT "http://elastic:changeme@localhost:9200/_cluster/settings?pretty" -H 'Content-Type: application/json' -d'
 {
-  "persistent": {
-    "cluster.routing.allocation.disk.watermark.low": "90%",
-    "cluster.routing.allocation.disk.watermark.high": "95%",
-    "cluster.routing.allocation.disk.watermark.flood_stage": "97%"
-  }
+    "persistent" : {
+      "cluster.routing.allocation.disk.threshold_enabled" : false
+    }
 }
 '
-
-curl -X PUT "http://elastic:changeme@localhost:9200/*/_settings?expand_wildcards=all&pretty" -H 'Content-Type: application/json' -d'
-{
-  "index.blocks.read_only_allow_delete": null
-}
-'
-
 
 env ELASTICSEARCH_IMAGE_REF=$1 ELASTIC_AGENT_IMAGE_REF=$1 KIBANA_IMAGE_REF=$1 elastic-package stack up -d -v --version $1 --services "elastic-agent"
 
