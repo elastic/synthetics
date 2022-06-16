b8c0ba6cf8dbb1d5e0a228a772d281ffb667441e
Dominique Clarke <dominique.clarke@elastic.co>
2022-06-16 09:23:42 -0400

    remove exit status 1

--- __tests__/e2e/scripts/setup_integration.sh
+++ __tests__/e2e/scripts/setup_integration.sh
@@ -23,14 +23,10 @@ curl -X PUT "http://elastic:changeme@localhost:9200/_cluster/settings?pretty" -H
 
 env ELASTICSEARCH_IMAGE_REF=$1 ELASTIC_AGENT_IMAGE_REF=$1 KIBANA_IMAGE_REF=$1 elastic-package stack up -d -v --version $1
 
-status=$?
-
 if [ $status -eq 1 ]; then
     echo "Fetching Fleet server logs... \n$(docker logs elastic-package-stack_fleet-server_1)"
 
     echo "Fetching Elastic Agent logs... \n$(docker logs elastic-package-stack_elastic-agent_1)"
 
     echo "Fetching Kibana logs... \n$(docker logs elastic-package-stack_kibana_1)"
-
-    exit $status
 fi
