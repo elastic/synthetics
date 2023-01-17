85957b0f91044299fc3a68a96216586cf99d7d4e
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2023-01-17 06:48:04 -0800

    remove skipped test

--- __tests__/push/index.test.ts
+++ __tests__/push/index.test.ts
@@ -61,8 +61,7 @@ describe('Push', () => {
     await rm(PROJECT_DIR, { recursive: true, force: true });
   });
 
-  // skipped for now, needs to get a project setup with no auth
-  it.skip('error when auth is ignored', async () => {
+  it('error when auth is ignored', async () => {
     const output = await runPush([]);
     expect(output).toContain(`required option '--auth <auth>' not specified`);
   });
