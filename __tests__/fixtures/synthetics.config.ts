f767e6a2d1eb056a76f739efdb278b82b8ee56e6
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2023-05-05 13:59:14 -0700

    fix tests

--- __tests__/fixtures/synthetics.config.ts
+++ __tests__/fixtures/synthetics.config.ts
@@ -35,9 +35,8 @@ module.exports = env => {
       ...devices['Galaxy S9+'],
     },
   };
-  if (env !== 'development' && config.params && config.monitor) {
+  if (env !== 'development' && config.params) {
     config.params.url = 'non-dev';
-    config.monitor.throttling = { latency: 11 };
   }
   return config;
 };
