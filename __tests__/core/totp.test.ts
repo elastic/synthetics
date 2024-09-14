bad06421f7963f89d97b2f31e5bf9919780a8fda
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2024-09-13 17:36:26 -0700

    reword and add test

--- __tests__/core/totp.test.ts
+++ __tests__/core/totp.test.ts
@@ -23,43 +23,15 @@
  *
  */
 
-import { TOTP } from "otpauth";
+import { createTOTP } from '../../src/core/totp';
 
-type TOTPOptions = {
-  /**
-   * Provider or Service the secret is associated with
-   */
-  issuer?: string
-  /**
-   * Account Identifier.
-   * @default 'SyntheticsTOTP'
-   */
-  label?: string
-  /**
-  * Include issuer prefix in label.
-  */
-  issuerInLabel?: boolean
-  /**
-   * The encoded secret key used to generate the TOTP.
-   */
-  secret?: string
-  /**
-   * The algorithm used to generate the TOTP.
-   * @default 'SHA1'
-   */
-  algorithm?: string
-  /**
-   * Number of digits in the generated token.
-   * @default 6
-   */
-  digits?: number
-  /**
-   * Validity period in seconds for the token.
-   * @default 30
-   */
-  period?: number
-};
+describe('TOTP', () => {
+  it('generate one time token', () => {
+    const totp = createTOTP({ secret: "FLIIOLP3IR3W" });
+    expect(totp.generate().length).toBe(6);
+    expect(totp.toString()).toEqual('otpauth://totp/SyntheticsTOTP?secret=FLIIOLP3IR3Q&algorithm=SHA1&digits=6&period=30')
+  });
+});
 
-export function createTOTP(options: TOTPOptions) {
-  return new TOTP({ label: "SyntheticsTOTP", ...options });
-}
+
+
